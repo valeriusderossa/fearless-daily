218e0b767839d5f443b4f4b324eea344fb3cc87e
Kamil <valeriusderossa@gmail.com>
2025-03-09 21:21:09 +0100

    init

---
+++ fearless-daily-be/src/main/java/com/kamil/fearlessdailybe/FearlessDailyBeApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.fearlessdailybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FearlessDailyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FearlessDailyBeApplication.class, args);
+    }
+
+}
---
+++ fearless-daily-be/src/test/java/com/kamil/fearlessdailybe/FearlessDailyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.fearlessdailybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FearlessDailyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
